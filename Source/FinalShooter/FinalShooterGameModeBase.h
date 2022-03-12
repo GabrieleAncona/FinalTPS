f0707e8e23f2fe3b2683467a72ba912807ad9f8a
GabrieleAncona <gabry.ancona@gmail.com>
2022-03-12 16:04:56 +0100

    Project setup

---
+++ Source/FinalShooter/FinalShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FinalShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALSHOOTER_API AFinalShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
